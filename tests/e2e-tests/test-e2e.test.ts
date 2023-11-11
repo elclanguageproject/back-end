8e8128839e78a5cc531b59b543cd474d1b6dcfee
Hankus Pankus <henryjacobscode@gmail.com>
2023-11-11 13:33:48 -0500

    Write some sample tests

---
+++ tests/e2e-tests/test-e2e.test.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+describe('E2E functionality working as expected', () => {
+  it('Calls the root url for the server and json message', async () => {
+    try {
+      let request = await axios.get(process.env.BACKEND_URL as string);
+      let data = request.data;
+      expect(data?.active).toBe(true);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+});
